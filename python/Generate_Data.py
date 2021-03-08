bb1297b901c4599a1258f6d306a2907091864e9e
wpmcgrath95 <wpmcgrath95@gmail.com>
2021-03-08 00:11:09 -0800

    added methods to python file

--- python/Generate_Data.py
+++ python/Generate_Data.py
@@ -5,6 +5,12 @@ class GenerateFakeData(object):
     def __init__(self):
         self.this_dir = os.path.dirname(os.path.realpath(__file__))
 
+    def generate_dates(sefl):
+        pass
+
+    def generate_ids(self):
+        pass
+
     def main(self):
         pass
     
