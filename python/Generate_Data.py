0066943f63a36fa255d6905f2ca6f29ea707cd99
wpmcgrath95 <wpmcgrath95@gmail.com>
2021-03-15 23:02:34 -0700

    generate ids

--- python/Generate_Data.py
+++ python/Generate_Data.py
@@ -5,11 +5,12 @@ class GenerateFakeData(object):
     def __init__(self):
         self.this_dir = os.path.dirname(os.path.realpath(__file__))
 
-    def generate_dates(sefl):
+    def generate_dates(self):
         pass
 
-    def generate_ids(self):
-        pass
+    def generate_ids(self, df, start, stop, **args):
+        ids = [hash(i)**2 for i in range(start, stop)]
+        return ids
 
     def main(self):
         pass
